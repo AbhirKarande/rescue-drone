05372e93e6de4a4c1149498afe1dc58da229433a
AbhirHI <33736838+AbhirKarande@users.noreply.github.com>
2022-11-19 15:20:10 -0500

    subscribing to tower position and transforming it to world coordinates

---
+++ src/simple_control/src/transformationAndPlanning.py
@@ -0,0 +1,42 @@
+#look up the transform between the coordinate frames for the cell_tower and the world
+#this is the transform from the cell_tower to the world
+
+#take the dog's last known position from /cell_tower/position and transform it to the world frame
+
+
+import rospy
+import tf2_ros
+import time
+import copy
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import PointStamped
+from tf2_geometry_msgs import do_transform_point
+
+class transformationAndPlanning:
+    def __init__(self):
+        time.sleep(10)
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        self.dog = Vector3()
+        self.dog_sub = rospy.Subscriber('/cell_tower/position', Vector3, self.dog_callback)
+
+        self.mainloop()
+    def mainloop(self):
+        rate = rospy.Rate(2)
+        while not rospy.is_shutdown():
+            if self.dog and (self.dog.x != 0 or self.dog.y != 0):
+                print('SELF.DOG', self.dog)
+                try: 
+                    lookup = self.tfBuffer.lookup_transform('world', 'cell_tower', rospy.Time.now())
+                    print('LOOKUP', lookup)
+                    goal_point = PointStamped("cell_tower",self.dog)
+                    print("GOAL_POINT TYPE", type(goal_point))
+                    print("GOAL_POINT", goal_point)
+                    goal_point1 = do_transform_point(goal_point, lookup)
+                    print('GOAL POINT',goal_point1)
+                    self.dog = Vector3(int(goal_point1.point.x), int(goal_point1.point.y), int(goal_point1.point.z))
+                    print('SELF.DOG', self.dog)
+                except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                    continue
+            rate.sleep()
+        
