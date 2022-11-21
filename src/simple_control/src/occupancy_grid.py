2bc00648bf1c38c7c23b5e1869eab240c1831d6e
AbhirHI <33736838+AbhirKarande@users.noreply.github.com>
2022-11-21 15:51:22 -0500

    occupancy grid additions, still need to figure out how to traverse map and use the lidar subscription to fill in the grid

--- src/simple_control/src/occupancy_grid.py
+++ src/simple_control/src/occupancy_grid.py
@@ -8,22 +8,24 @@ from nav_msgs.msg import OccupancyGrid
 class occupancy_grid:
     def __init__(self):
         self.lidar_reading = Vector3()
-        self.lidar_sub = rospy.Subscriber('/lidar_reading', Vector3, self.lidar_callback)
-        self.occupancy_grid = rospy.Publisher('/map', OccupancyGrid, queue_size=10)
+        self.lidar_sub = rospy.Subscriber('/uav/sensors/lidar', Vector3, self.lidar_callback)
+        self.drone_pos = Vector3()
+        self.drone_pos_sub = rospy.Subscriber('/uav/sensors/gps', Vector3, self.drone_pos_callback)
+        self.occupancy_grid = OccupancyGrid()
+        #initialize all cells of the occupancy grid to 50
+        self.occupancy_grid.data = [50 for i in range(100)]
+        self.occupancy_grid_pub = rospy.Publisher('/map', OccupancyGrid, queue_size=10)
         self.mainloop()
     def lidar_callback(self, data):
         self.lidar_reading = data
-
+    def drone_pos_callback(self, data):
+        self.drone_pos = data
     def mainloop(self):
         #rate = rospy.Rate(2)
         while not rospy.is_shutdown():
-            #get each of the lidar readings around the drone and determine if there is an obstacle or not
-            #if there is an obstacle, update the occupancy grid
-
             if self.lidar_reading and (self.lidar_reading.x != 0 or self.lidar_reading.y != 0):
                 
-                self.occupancy_grid.publish(self.lidar_reading)
-            #if there is not an obstacle, update the occupancy grid
-            #rate.sleep()
-            pass
+                #update the current cell (drone's position)of the occupancy grid to 100
+                
+                self.occupancy_grid.data[] = 100
 
