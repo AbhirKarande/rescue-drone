a94ecf21401e46d41c3a423fef4bb22d69fa9cf5
AbhirHI <33736838+AbhirKarande@users.noreply.github.com>
2022-11-25 14:59:34 -0500

    testing out lidar readings in transformation since it is already a functioning node

--- src/simple_control/src/transformation.py
+++ src/simple_control/src/transformation.py
@@ -20,26 +20,32 @@ class transformation:
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
         self.dog = Vector3()
+        self.lidar = Vector3()
         self.dog_sub = rospy.Subscriber('/cell_tower/position', Vector3, self.dog_callback)
+        self.lidar_sub = rospy.Subscriber('/uav/sensors/lidar', Vector3, self.lidar_callback)
 
         self.mainloop()
     def dog_callback(self, data):
         self.dog = data
+    def lidar_callback(self, data):
+        self.lidar = data
     def mainloop(self):
         rate = rospy.Rate(2)
         while not rospy.is_shutdown():
             if self.dog and (self.dog.x != 0 or self.dog.y != 0):
                 print('SELF.DOG', self.dog)
                 try: 
+                    print(self.lidar)
                     lookup = self.tfBuffer.lookup_transform('world', 'cell_tower', rospy.Time.now())
-                    print('LOOKUP', lookup)
+                    #print('LOOKUP', lookup)
                     goal_point = PointStamped("cell_tower",self.dog)
-                    print("GOAL_POINT TYPE", type(goal_point))
-                    print("GOAL_POINT", goal_point)
+                    #print("GOAL_POINT TYPE", type(goal_point))
+                    #print("GOAL_POINT", goal_point)
                     goal_point1 = do_transform_point(goal_point, lookup)
-                    print('TRANSFORMED DOG POINT',goal_point1)
+                    #print('TRANSFORMED DOG POINT',goal_point1)
                     self.dog = Vector3(int(goal_point1.point.x), int(goal_point1.point.y), int(goal_point1.point.z))
-                    print('SELF.DOG', self.dog)
+                    #print('SELF.DOG', self.dog)
+
                 except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                     continue
             rate.sleep()
