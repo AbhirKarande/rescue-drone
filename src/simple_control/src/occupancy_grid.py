296fecdcca85ef83ca5c53a97a3e1fa2cdac00ca
AbhirHI <33736838+AbhirKarande@users.noreply.github.com>
2022-11-28 20:04:33 -0500

    occupancy grid now overlays on map but is still messed up

--- src/simple_control/src/occupancy_grid.py
+++ src/simple_control/src/occupancy_grid.py
@@ -3,28 +3,39 @@
 import rospy
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
 import tf2_ros
 from tf2_geometry_msgs import do_transform_point
+import math
 class occupancy_grid:
     def __init__(self):
         self.lidar_reading = LaserScan()
         self.lidar_sub = rospy.Subscriber('/uav/sensors/lidar', LaserScan, self.lidar_callback)
-        self.drone_pos = Vector3()
-        self.drone_pos_sub = rospy.Subscriber('/uav/sensors/gps', Vector3, self.drone_pos_callback)
-        self.drone_pos_pub = rospy.Publisher('/uav/input/position', Vector3, queue_size=1)
+        self.drone_pos = PoseStamped()
+        self.drone_pos_sub = rospy.Subscriber('/uav/sensors/gps', PoseStamped, self.drone_pos_callback)
+        #self.drone_pos_pub = rospy.Publisher('/uav/input/position', Vector3, queue_size=1)
         self.occupancy_grid = OccupancyGrid()
         self.dog = Vector3()
-        self.dog_sub = rospy.Subscriber('/cell_tower/position', Vector3, self.dog_callback)
+        #self.dog_sub = rospy.Subscriber('/cell_tower/position', Vector3, self.dog_callback)
         #initialize all cells of the occupancy grid to 50
-        self.occupancy_grid.data = [50 for i in range(100)]
+        self.occupancy_grid.data = [50 for i in range(121)]
+        self.occupancy_grid.info.map_load_time = rospy.Time.now()
+        self.occupancy_grid.header.frame_id = 'world'
+        self.occupancy_grid.header.stamp = rospy.Time.now()
+        
+        self.occupancy_grid.info.resolution = 1
+        self.occupancy_grid.info.width = 22
+        self.occupancy_grid.info.height = 22
+        self.occupancy_grid.info.origin.position.x = self.drone_pos.pose.position.x
+        self.occupancy_grid.info.origin.position.y = self.drone_pos.pose.position.y
         self.occupancy_grid_pub = rospy.Publisher('/map', OccupancyGrid, queue_size=10)
         self.mainloop()
     def lidar_callback(self, data):
         self.lidar_reading = data
-    def dog_callback(self, data):
-        self.dog = data
+    # def dog_callback(self, data):
+    #     self.dog = data
     def drone_pos_callback(self, data):
         self.drone_pos = data
         
@@ -32,21 +43,20 @@ class occupancy_grid:
     def mainloop(self):
         rate = rospy.Rate(2)
         while not rospy.is_shutdown():
-            if self.dog and (self.dog.x != 0 or self.dog.y != 0):
-                try: 
-                    print('SELF.LIDAR', self.lidar)
-                    lookup = self.tfBuffer.lookup_transform('world', 'cell_tower', rospy.Time.now())
-                    #print('LOOKUP', lookup)
-                    goal_point = PointStamped("cell_tower",self.dog)
-                    #print("GOAL_POINT TYPE", type(goal_point))
-                    #print("GOAL_POINT", goal_point)
-                    goal_point1 = do_transform_point(goal_point, lookup)
-                    #print('TRANSFORMED DOG POINT',goal_point1)
-                    self.dog = Vector3(int(goal_point1.point.x), int(goal_point1.point.y), int(goal_point1.point.z))
-                    #print('SELF.DOG', self.dog)
+            # if self.dog and (self.dog.x != 0 or self.dog.y != 0):
+            #     try: 
+            #         lookup = self.tfBuffer.lookup_transform('world', 'cell_tower', rospy.Time.now())
+            #         #print('LOOKUP', lookup)
+            #         goal_point = PointStamped("cell_tower",self.dog)
+            #         #print("GOAL_POINT TYPE", type(goal_point))
+            #         #print("GOAL_POINT", goal_point)
+            #         goal_point1 = do_transform_point(goal_point, lookup)
+            #         #print('TRANSFORMED DOG POINT',goal_point1)
+            #         self.dog = Vector3(int(goal_point1.point.x), int(goal_point1.point.y), int(goal_point1.point.z))
+            #         #print('SELF.DOG', self.dog)
 
-                except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-                    continue
+            #     except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            #         continue
             #get the angle of the first ray
             angle = self.lidar_reading.angle_min
             #iterate over all the rays
@@ -54,27 +64,37 @@ class occupancy_grid:
                 #get the distance of the ray
                 distance = self.lidar_reading.ranges[i]
                 #calculate the x and y coordinates of the ray
-                x = self.drone_pos.x + distance * math.cos(angle)
-                y = self.drone_pos.y + distance * math.sin(angle)
+                x = self.drone_pos.pose.position.x + distance * math.cos(angle)
+                y = self.drone_pos.pose.position.y + distance * math.sin(angle)
+                index = x + y * 10
+                self.occupancy_grid.data[int(index)] = 100
+                print('X', x)
+                print('Y', y)
+                print('INDEX: ',int(index))
+                # emptyDistance = distance
+                # while emptyDistance > 0:
+                #     emptyDistance = distance - 0.5
 
-                emptyDistance = distance - 0.5
-                emptyX = self.drone_pos.x + emptyDistance * math.cos(angle)
-                emptyY = self.drone_pos.y + emptyDistance * math.sin(angle)
+                #     emptyX = self.drone_pos.pose.position.x + emptyDistance * math.cos(angle)
+                #     emptyY = self.drone_pos.pose.position.y + emptyDistance * math.sin(angle)
+                #     emptyIndex = int(emptyX) + int(emptyY) * 10
+                #     self.occupancy_grid.data[emptyIndex] = 0
+                
+
+
 
 
 
                 #calculate the index of the cell in the occupancy grid
-                index = int(x) + int(y) * 10
-                emptyIndex = int(emptyX) + int(emptyY) * 10
+                
                 #set the cell to 100
-                self.occupancy_grid.data[index] = 100
-                self.occupancy_grid.data[emptyIndex] = 0
                 #increment the angle
                 angle += self.lidar_reading.angle_increment
-            #TODO: 
-            self.drone_pos_pub.publish(self.dog)
+            #move the drone to where there is empty space
+            
             
             #publish the occupancy grid
+            print(self.occupancy_grid.data)
             self.occupancy_grid_pub.publish(self.occupancy_grid)
             rate.sleep()
 if __name__ == '__main__':
